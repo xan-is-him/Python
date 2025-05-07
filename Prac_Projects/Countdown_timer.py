06df394d939d703976cd8fc3954912f57c934117
Sujit Lama <lamasujit11@gmail.com>
2025-05-07 18:21:28 +0545

    Add files via upload

---
+++ Prac_Projects/Countdown_timer.py
@@ -0,0 +1,16 @@
+#Countdown timer
+
+import time
+
+my_time = int(input("Enter time in seconds: "))
+
+for x in range(my_time, 0, -1): #using step for reverse
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+
+
+print("Time's UP!")
